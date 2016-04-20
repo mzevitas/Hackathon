1c49ceefc0355c6e3d5cdc68fde192b336983775
Mike Zevitas <mzevitas@gmail.com>
2016-04-20 19:39:41 -0400

    background styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,46 +4,5 @@ $('#toggle, nav a').click(function() {
    $('#overlay').toggleClass('open');
   });
 
-            // var scroll_pos = 0;
-            // $(document).scroll(function() { 
-            //     scroll_pos = $(this).scrollTop();
-            //     if(scroll_pos > 210) {
-            //         $("header").css({
-            //         	'transition': '1s ease',
-            //         	'background-color': 'hsla(0,0%,0%,0.3)',
-            //         	'border-bottom': '.5px ridge hsla(209, 49%, 68%, 1)',
-            //         	'height': '70px',
-
-            //         });
-            //         $("header img").css({
-            //             'width': '40px',
-            //             'transition': '1s ease'
-            //         });
-            //         $(".button_container").css({
-            //             'width': '30px',
-            //             'height': '22px',
-            //             'transition': '1s ease',
-            //             'margin-top': '-10px'
-            //         });
-
-
-            //     } else {
-            //         $("header").css({
-            //             'background-color': 'transparent',
-            //             'border-bottom': 'none',
-            //             'color': '#000000'
-            //         });
-            //         $("header img").css({
-            //         	'width': '50px',
-            //         	'transition': '.5s ease'
-            //         });
-            //         $(".button_container").css({
-            //         	'width': '35px',
-            //         	'height': '27px',
-            //         	'transition': '.5s ease',
-            //         	// 'padding-top': '5px'
-            //         });
-            //     }
-            // });
         });
 
